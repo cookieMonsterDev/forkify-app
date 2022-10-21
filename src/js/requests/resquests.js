9629652a396ea9208d13ea61ed29fa9c88356085
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2022-10-22 00:26:54 +0100

    feat: setup

--- src/js/requests/resquests.js
+++ src/js/requests/resquests.js
@@ -4,15 +4,22 @@ const appRequests = axios.create({
   baseURL: 'https://forkify-api.herokuapp.com/api/v2/recipes',
 });
 
-export const getSingleRecipe = async (id) => {
+export const getSingleRecipe = async id => {
   try {
     const res = await appRequests.get(id);
+    const body = res.data.data.recipe;
 
-    if(!res) throw new Error(`Sth 1`)
-
-    return res.data.data
+    return {
+      id: body.id,
+      title: body.title,
+      publisher: body.publisher,
+      sourceUrl: body.source_url,
+      image: body.image_url,
+      servings: body.servings,
+      cookingTime: body.cooking_time,
+      ingredients: body.ingredients,
+    };
+  } catch (err) {
+    throw new Error(`Something went worng!`);
   }
-  catch(err) {
-    throw new Error(`Sth 2`)
-  }
-}
+};
