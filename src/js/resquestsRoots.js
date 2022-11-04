b6d85bd5df86673c04b8f686b2057b6eb115cfcd
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2022-11-04 18:17:20 +0200

    feat: creat react app

--- src/js/resquestsRoots.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { BASE_URL, TIMEOUT } from './config';
-
-export const appRequests = axios.create({
-  baseURL: BASE_URL,
-});
-
-appRequests.defaults.timeout = TIMEOUT;
-appRequests.defaults.timeoutErrorMessage = 'timeout 🤬🤬🤬';
